5ef6374274e2b19d165c204f7ddb27e578b3423e
nero58 <nero58.deepak@gmail.com>
2024-08-09 10:44:57 +0530

    update req.txt

--- gesBackendApi/gesBackendApi/settings.py
+++ gesBackendApi/gesBackendApi/settings.py
@@ -67,7 +67,8 @@ MIDDLEWARE = [
     # # 'restrictAdmin.views.RestrictStaffToAdminMiddleware',
     # # 'allauth.account.middleware.AccountMiddleware',
     # # 'django_hosts.middleware.HostsResponseMiddleware'
-      "django.middleware.security.SecurityMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -183,8 +184,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
