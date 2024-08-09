66e3ef6c8d23ec47da8142108b4220f359db59cc
nero58 <nero58.deepak@gmail.com>
2024-08-09 08:46:58 +0530

    connect db

--- gesBackendApi/gesBackendApi/settings.py
+++ gesBackendApi/gesBackendApi/settings.py
@@ -132,7 +132,7 @@ WSGI_APPLICATION = 'gesBackendApi.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default=os.getenv("DATABASE_RENDER_DB"),
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600
     )
 }
